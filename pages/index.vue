29faa550235b49a9cb7006c88af4cc585c0ecabb
Happy <oncwnuODv4nCnT1pQ88NziO_Cuk0@git.weixin.qq.com>
2024-02-17 16:47:28 +0800

    fix blur

--- pages/index.vue
+++ pages/index.vue
@@ -8,21 +8,15 @@ const blogStore = useBlogStore()
 <template>
 
   <div class="flex flex-col justify-center items-center font-frank-ruhl-libre">
-    <div>
-      <div class="absolute top-[-10px] right-[-50px] " >
-        <nuxt-img class="h-[100px] w-[300px] blur-[50px] " src="/images/Blur.svg"></nuxt-img>
+    <div class="w-full h-full overflow-hidden absolute ">
+      <div
+          class="top-[80px] left-[50px] rotate-[-45deg] w-[200px] h-[620px] blur-[200px] absolute super-blur bg-primary -z-50">
       </div>
-<!--      <div-->
-<!--          class="top-[80px] left-[50px] rotate-[-45deg] w-[200px] h-[620px] blur-[200px] absolute super-blur bg-primary -z-50">-->
-<!--      </div>-->
 
-<!--      <div-->
-<!--          class="top-[-200px] right-[100px] rotate-[-80deg] w-[100px] h-[500px] blur-[200px] absolute super-blur bg-primary -z-50">-->
-<!--      </div>-->
+      <div
+          class=" top-[-200px] right-[100px] rotate-[-80deg] w-[100px] h-[500px] blur-[200px] absolute super-blur bg-primary -z-50">
+      </div>
 
-<!--      <div-->
-<!--          class="top-[200px] right-[300px] rotate-[30deg] w-[100px] h-[300px] blur-[200px] absolute super-blur bg-primary -z-50">-->
-<!--      </div>-->
     </div>
 
 
