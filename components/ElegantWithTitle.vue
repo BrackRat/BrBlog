450aca1581455a3e109d7d608e6ce7528426493a
Happy <oncwnuODv4nCnT1pQ88NziO_Cuk0@git.weixin.qq.com>
2024-02-18 21:30:32 +0800

    add

---
+++ components/ElegantWithTitle.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+
+const props = defineProps({
+  text: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  width: {
+    type: String,
+    required: true,
+  },
+  height: {
+    type: String,
+    required: true,
+  },
+  shadow_x: {
+    type: String,
+    required: true,
+  },
+  shadow_y: {
+    type: String,
+    required: true,
+  },
+  rounded: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="flex flex-col items-center">
+    <ShadowImage :image="image" :shadow_x="shadow_x" :shadow_y="shadow_y"
+                 :height="height" :width="width" :rounded="rounded"/>
+
+    <div class="text-content pt-8 text-xl ">
+      {{ text }}
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
