f8a335a44240125bb3a840de4db56db8636085b2
Happy <oncwnuODv4nCnT1pQ88NziO_Cuk0@git.weixin.qq.com>
2024-02-23 14:59:45 +0800

    traveling

--- components/navbar.vue
+++ components/navbar.vue
@@ -25,11 +25,17 @@ const switchRoutes = computed(() => {
 </script>
 
 <template>
-  <div class="flex p-4 justify-center flex-row space-x-4" >
-    <div v-for="(item, index) in switchRoutes" :key="index">
+  <div class="flex p-4  flex-row space-x-4" >
+    <div class="flex flex-grow"></div>
+
+    <div class="flex" v-for="(item, index) in switchRoutes" :key="index">
       <div :class="item.route === route.path ? 'font-frank-ruhl-libre text-lg hover:cursor-pointer text-primary underline' : 'font-frank-ruhl-libre text-lg hover:cursor-pointer text-secondary  hover:text-primary transition-all'">
         <NuxtLink  :to="item.route">{{ item.title }}</NuxtLink>
       </div>
+    </div>
+    <div class="flex flex-grow"></div>
+    <div class="justify-items-end">
+      <NuxtLink class="-ml-4" to="https://www.travellings.cn/go.html"><Icon class="text-secondary text-opacity-70 " name="mingcute:train-2-line"></Icon> </NuxtLink>
     </div>
   </div>
 </template>
